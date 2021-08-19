efebd3fb717ad9e7c4c28674460573272edb6866
Corey Adams <coreyadams16@protonmail.com>
2021-08-19 11:47:56 -0400

    Added BikeRental Class

--- PyBikeRental/classes/BikeRental.py
+++ PyBikeRental/classes/BikeRental.py
@@ -1,4 +1,121 @@
+import datetime
+from datetime import datetime, timedelta
+
 class BikeRental(object):
-    """description of class"""
+    """Functions to set up and get data from BikeRental Shop """
 
+    def __init__(self,stock=0):
+        """
+        Our constructor class that instantiates bike rental shop.
+        """
+        self.stock = stock 
 
+    def displaystock(self):
+        """
+        Displays the bikes currently available for rent in the shop.
+        """
+        print("We have currently {} bikes available to     rent.".format(self.stock))
+        return self.stock
+
+    def rentBikeOnHourlyBasis(self, n):
+        """
+        Rents a bike on hourly basis to a customer.
+        """
+        # reject invalid input 
+        if n <= 0:
+            print("Number of bikes should be positive!")
+            return None
+        # do not rent bike is stock is less than requested bikes
+        
+        elif n > self.stock:
+            print("Sorry! We have currently {} bikes available to rent.".format(self.stock))
+            return None
+        # rent the bikes        
+        else:
+            now = datetime.datetime.now()                      
+            print("You have rented a {} bike(s) on hourly basis today at {} hours.".format(n,now.hour))
+            print("You will be charged $5 for each hour per bike.")
+            print("We hope that you enjoy our service.")
+            self.stock -= n
+            return now      
+     
+    def rentBikeOnDailyBasis(self, n):
+        """
+        Rents a bike on daily basis to a customer.
+        """
+        
+        if n <= 0:
+            print("Number of bikes should be positive!")
+            return None
+        elif n > self.stock:
+            print("Sorry! We have currently {} bikes available to rent.".format(self.stock))
+            return None
+    
+        else:
+            now = datetime.now()                      
+            print("You have rented {} bike(s) on daily basis today at {} hours.".format(n, now.hour))
+            print("You will be charged $20 for each day per bike.")
+            print("We hope that you enjoy our service.")
+            self.stock -= n
+            return now
+        
+    def rentBikeOnWeeklyBasis(self, n):
+        """
+        Rents a bike on weekly basis to a customer.
+        """
+        if n <= 0:
+            print("Number of bikes should be positive!")
+            return None
+        elif n > self.stock:
+            print("Sorry! We have currently {} bikes available to rent.".format(self.stock))
+            return None        
+        
+        else:
+            now = datetime.now()
+            print("You have rented {} bike(s) on weekly basis today at {} hours.".format(n, now.hour))
+            print("You will be charged $60 for each week per bike.")
+            print("We hope that you enjoy our service.")
+            self.stock -= n
+            return now
+    
+    
+    def returnBike(self, request):
+        """
+        1. Accept a rented bike from a customer
+        2. Replensihes the inventory
+        3. Return a bill
+        """
+       
+        # extract the tuple and initiate bill
+        rentalTime, rentalBasis, numOfBikes = request
+        bill = 0
+        # issue a bill only if all three parameters are not null!
+        if rentalTime and rentalBasis and numOfBikes:
+            self.stock += numOfBikes
+            now = datetime.now()
+            rentalPeriod = now - rentalTime
+        
+            # hourly bill calculation
+            if rentalBasis == 1:
+                bill = round(rentalPeriod.seconds / 3600) * 5 * numOfBikes
+                
+            # daily bill calculation
+            elif rentalBasis == 2:
+                bill = round(rentalPeriod.days) * 20 * numOfBikes
+                
+            # weekly bill calculation
+            elif rentalBasis == 3:
+                bill = round(rentalPeriod.days / 7) * 60 * numOfBikes
+            
+            # family discount calculation
+            if (3 <= numOfBikes <= 5):
+                print("You are eligible for Family rental promotion of 30% discount")
+                bill = bill * 0.7
+            print("Thanks for returning your bike. Hope you enjoyed our service!")
+            print("That would be ${}".format(bill))
+            return bill
+        
+        else:
+            print("Are you sure you rented a bike with us?")
+            return None
+  
